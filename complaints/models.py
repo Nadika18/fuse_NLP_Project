a4e14b8738af51f126aa12874243318a063ee31c
Nadika Poudel <nadikapoudel16@gmail.com>
2023-12-14 22:02:22 +0545

    website created and word2vec model updated

--- complaints/models.py
+++ complaints/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+
 
 class TextComplaint(models.Model):
     complaint=models.CharField(max_length=100)
     complaint_time=models.DateTimeField(auto_now_add=True)
     predicted_class = models.CharField(max_length=100, blank=True, null=True)
+
+
+
--- complaints/views.py
+++ complaints/views.py
@@ -1,5 +1,28 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render, get_object_or_404,redirect
+from .models import TextComplaint
+from django.http import Http404
+import time
+from datetime import datetime
+from .predictor import make_prediction 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    complaints=TextComplaint.objects.all()
+    context={
+        'complaints':complaints
+    }
+    return render(request,'index.html',context=context)
+
+
+
+def complain(request):
+    if request.method=='POST':
+        complaint=request.POST['complaint']
+        complaint_time=datetime.now()
+        predicted_class = make_prediction(complaint)
+
+        # text_complaint=TextComplaint(complaint=complaint,complaint_time=complaint_time)
+        text_complaint=TextComplaint(complaint=complaint,complaint_time=complaint_time,predicted_class=predicted_class)
+        text_complaint.save()       
+        return redirect('/') 
+    return render(request,'complain.html')
